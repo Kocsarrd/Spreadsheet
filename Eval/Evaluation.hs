38dd01a56ae78d74edcd5784f0e5122b6d9eb07b
Márk Széles <mark.szeles@gmail.com>
2021-03-06 18:12:59 +0100

    .

---
+++ Eval/Evaluation.hs
@@ -0,0 +1,24 @@
+module Eval.Evaluation where
+
+import Control.Concurrent
+import Language.Haskell.Ghcid
+import System.Process
+
+import GUI.Types
+import Spreadsheet.Types
+
+execCommand :: EvalData -> String -> IO (Either EvalError [String])
+execCommand (EvalData ghciR commandR resultR) command = do
+  putMVar commandR command
+  mResult <- takeMVar resultR
+  case mResult of
+    Right result -> pure $ Right result
+    Left pid -> do
+      callProcess "kill" ["-SIGKILL", pid]
+      createGhci >>= swapMVar ghciR >> pure (Left ETimeoutError)
+      
+
+createGhci :: IO Ghci
+createGhci = do
+  ghci <- fst <$> startGhci "ghci" (Just ".") (\_ _ -> pure ())
+  pure ghci
